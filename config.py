7304248bc6c3b23519eef35027973abe2e4be7c9
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-03-31 18:31:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,20 @@
 from os import environ
 
 # Telegram Account Api Id And Api Hash
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
+API_ID = int(environ.get("API_ID", "27823209"))
+API_HASH = environ.get("API_HASH", "1d693fcf3bfea119ca1d9057b08a4495")
 
 # Your Main Bot Token 
 BOT_TOKEN = environ.get("BOT_TOKEN", "")
 
 # Owner ID For Broadcasting 
-OWNER_ID = int(environ.get("OWNER_ID", "")) # Owner Id or Admin Id
+OWNER_ID = int(environ.get("OWNER_ID", "6004928770")) # Owner Id or Admin Id
 
 # Give Your Force Subscribe Channel Id Below And Make Bot Admin With Full Right.
-F_SUB = environ.get("F_SUB", "")
+F_SUB = environ.get("F_SUB", "-1002327045567")
 
 # Mongodb Database Uri For User Data Store 
-MONGO_DB_URI = environ.get("MONGO_DB_URI", "")
+MONGO_DB_URI = environ.get("MONGO_DB_URI", "mongodb+srv://kalitgstringsession:kalitgstringsession@cluster0.rby7z6u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 
 # Port To Run Web Application 
 PORT = int(environ.get('PORT', 8080))
