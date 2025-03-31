51bbc3b2fb7e85c3086b72d5391a1d2389be3cd9
Kalitg28 <157677832+Kalitg28@users.noreply.github.com>
2025-03-31 18:55:38 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_ID = int(environ.get("API_ID", "27823209"))
 API_HASH = environ.get("API_HASH", "1d693fcf3bfea119ca1d9057b08a4495")
 
 # Your Main Bot Token 
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7963199045:AAF56QGnfSRVlxz-Oh3O02Geo-cJ_hCUxGM")
 
 # Owner ID For Broadcasting 
 OWNER_ID = int(environ.get("OWNER_ID", "6004928770")) # Owner Id or Admin Id
